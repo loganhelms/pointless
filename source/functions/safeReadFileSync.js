865f0d789c852805ca02fc936db07f14d1205095
Logan Helms <logan.a.helms@gmail.com>
2018-08-20 21:43:35 -0400

    Add safeReadFileSync

--- source/functions/safeReadFileSync.js
+++ source/functions/safeReadFileSync.js
@@ -1,10 +1,21 @@
 const fs        = require('fs')
 const { curry } = require('ramda')
+const Either    = require('../types/either')
 
-const readFileSync =
-  curry((encoding, filename) =>
-    fs.readFileSync(`${__dirname}/${filename}`, encoding))
+/**
+ * Reads contents from a file
+ *
+ * @sig
+ * @param {String} encoding The encoding
+ * @param {String} filename The filename including extention
+ * @return {Eiter} Results of read
+ */
+const safeReadFileSync = curry((encoding, filename) => {
+  try {
+    return Either.fromNullable(fs.readFileSync(`${__dirname}/${filename}`, encoding))
+  } catch (error) {
+    return Either.left(`${error.name}: ${error.message}`)
+  }
+})
 
-module.exports = readFileSync
-
-console.log(readFileSync('utf8')('myfile.txt'))
+module.exports = safeReadFileSync
