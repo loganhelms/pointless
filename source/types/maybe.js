3ff222a4eca231b2f5e185da029617bfad24fc4f
Logan Helms <logan.a.helms@gmail.com>
2018-08-20 21:42:48 -0400

    Fix circular reference issues

--- source/types/maybe.js
+++ source/types/maybe.js
@@ -1,6 +1,4 @@
 const { isNil } = require('ramda')
-const Just      = require('./just')
-const Nothing   = require('./nothing')
 
 class Maybe {
   get isNothing()         { return false          }
@@ -14,6 +12,33 @@ class Maybe {
                               ? Maybe.nothing()
                               : Maybe.just(a)
                           }
+}
+
+class Just extends Maybe {
+  constructor(value) {
+    super()
+    this._value = value
+  }
+
+  get value() { return this._value                        }
+  getOrElse() { return this._value                        }
+  chain(f)    { return f(this._value)                     }
+  map(f)      { return Maybe.fromNullable(f(this._value)) }
+  filter(f)   {
+                Maybe.fromNullable(f(this._value)
+                ? this._value
+                : null)
+              }
+  toString()  { return `Maybe.Just(${this._value})`       }
+}
+
+class Nothing extends Maybe {
+  get value()       { throw new TypeError('Cannot extract the value of a Nothing.') }
+  getOrElse(other)  { return other                                                  }
+  chain(f)          { return this                                                   }
+  map(f)            { return this                                                   }
+  filter(f)         { return this._value                                            }
+  toString()        { return `Maybe.Nothing`                                        }
 }
 
 module.exports = Maybe
